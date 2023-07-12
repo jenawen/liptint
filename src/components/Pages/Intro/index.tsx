cfed080891e9448cf400f2b8b779671c47c4d433
jena adkins <jena.adkins@creditone.com>
2023-07-12 14:38:32 -0700

    working on page scrolling

--- src/components/Pages/Intro/index.tsx
+++ src/components/Pages/Intro/index.tsx
@@ -1,11 +1,21 @@
 import "./index.css";
+import { useEffect, useState } from "react";
 
-const Intro = () => {
+const Intro = (props: any) => {
+  const { setCurrent } = props;
+
   return (
     <div className="intro-wrapper">
       <div className="intro-text">
         <div className="intro-header">What are we wearing today ?</div>
         <div>Let me help you pick your lip color!</div>
+        <div
+          onClick={() => {
+            setCurrent(2);
+          }}
+        >
+          Let's go!
+        </div>
       </div>
     </div>
   );
--- src/components/Wrapper/index.tsx
+++ src/components/Wrapper/index.tsx
@@ -1,11 +1,34 @@
 import Intro from "../Pages/Intro";
 import Questions from "../Pages/Questions";
+import { useEffect, useState } from "react";
 
 const Wrapper = () => {
+  const [current, setCurrent] = useState(1);
+  const handleOnClick = (btnNumber: number) => {
+    setCurrent(btnNumber);
+  };
+
+  useEffect(() => {
+    if (current === 3) {
+      setCurrent(1);
+    }
+    if (document) {
+      document
+        .querySelector(`#slide-${current > 3 ? 1 : current}`)!
+        .scrollIntoView();
+    }
+  }, [current]);
+
   return (
     <div className="page-container">
-      <Intro />
-      <Questions />
+      <div id="slide-1">
+        {" "}
+        <Intro setCurrent={setCurrent} />{" "}
+      </div>
+      <div id="slide-2">
+        {" "}
+        <Questions />{" "}
+      </div>
     </div>
   );
 };
