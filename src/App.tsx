c4cf445ceb421446ba28a61a99a7e3d4b418f00b
Jena <heartfilia12@gmail.com>
2023-08-08 22:30:26 -0700

    added json db, fetch function

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import Wrapper from "./components/Wrapper";
 
+import { useState, useEffect } from "react";
+
 function App() {
+  const [quiz, setQuiz] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:1573/quiz");
+      const json = await response.json();
+      setQuiz(json);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
       <Wrapper />
